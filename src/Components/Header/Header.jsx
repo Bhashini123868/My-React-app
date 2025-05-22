e29f14d2931904e7c3cd041e5ec1196f6c3be1ae
Bhashini123868 <bhashimunasinghe286@gmail.com>
2025-05-22 13:40:41 +0530

    react image

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './Header.css'
 import MenuLink from '../MenuLink/MenuLink'
+import Logoimage from '../../assets/react.svg'
 
 function Header() {
   return (
     <div id="navcontent">
         {/* <h3>Header content</h3> */}
         {/* <a href="http://www.react.dev"><img src={logo} alt="React"/></a> */}
+
+        <img src="vite.svg" alt='Vite Logo'/>
+        <img src={Logoimage} alt=''/>
 
         <div>
           <MenuLink linkname="Home" url="#home"/>
