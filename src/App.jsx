77cc423f6a87079b394e6187b98ac5d59dc8de1b
Bhashini123868 <bhashimunasinghe286@gmail.com>
2025-05-23 17:14:39 +0530

    react click function

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,16 @@ import Header from "./Components/Header/Header"
 
 function App() {
 
+  function hello1(){
+    console.log("hello 1");
+  }
+  const hello2 = () => {
+    console.log("hello 2");
+  }
+  function hello3(name){
+    console.log("hello 3 " + name);
+  }
+
   return (
     <>
     {/* <h1 className="heading">React</h1>
@@ -17,7 +27,9 @@ function App() {
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor ad culpa sapiente iste quod labore fugit odit voluptatibus saepe praesentium mollitia suscipit, a itaque amet nobis qui odio architecto eum!</p>
     </Body>
     <Footer/>
-    <button>Click 1</button>
+    <button onClick={hello1}>Click 1</button>
+    <button onClick={hello2}>Click 2</button>
+    <button onClick={() => hello3("Bhashini")}>Click 3</button>
 
     </div>
     </>
